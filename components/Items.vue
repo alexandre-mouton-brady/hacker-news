c6025604f8e0c5a2940ebbd4bbe8d570298efe6c
unknown <amoutonbrady@gmail.com>
2017-10-24 19:58:25 +0200

    Added style

--- components/Items.vue
+++ components/Items.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="code">
     <ul class="list pa2">
-      <li v-for="item in items" :key="item.id">{{item.title}}</li>
+      <li class="list__item" v-for="item in items" :key="item.id">
+        <div class="score">{{item.score}}</div>
+        <div class="title">{{item.title}}</div>
+        <div class="details">by {{item.by}} - {{item.time}}</div>
+        <div class="comments">{{item.descendants}} comments</div>
+      </li>
     </ul>
   </div>
 </template>
@@ -14,3 +19,37 @@ export default {
 }
 </script>
 
+<style scoped>
+.list__item {
+  display: grid;
+  grid: repeat(4, 1.5em) / repeat(10, 1fr);
+  grid-row-gap: 1em;
+}
+
+.score {
+  grid-row: 1 / -1;
+  grid-column: span 1;
+  align-self: center;
+  justify-self: center;
+}
+
+.title {
+  grid-row: 1 / 3;
+  grid-column: 2 / -2;
+  align-self: end;
+}
+
+.details,
+.comments {
+  grid-row: 3 / -1;
+}
+
+.comments {
+  grid-column: 2 / 6;
+}
+
+.details {
+  grid-column: 6 / -2;
+  justify-self: end;
+}
+</style>
