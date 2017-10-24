c16b297ad384b0360d318ceb8c0f2342421e0fb6
unknown <amoutonbrady@gmail.com>
2017-10-24 18:54:37 +0200

    Added async data fetching on start

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
+  <div class="f1 code">
     Hello world!
+    <hr>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{user.login}}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  asyncData() {
+    return axios.get('users').then(res => ({
+      users: res.data
+    }))
+  }
+}
+</script>
 
